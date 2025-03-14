03454b40e1e4a9ebbf24a41f44c414cb8899b00d
zhuzhuc <1727643988@qq.com>
2025-03-14 09:28:32 +0800

    zz_os: Freestangding Rust Binary

---
+++ zz_os/target/debug/deps/zz_os-2fd76523e1f9f4f6.d
@@ -0,0 +1,5 @@
+/Users/Apple/Desktop/Some/my-rust-os/zz_os/target/debug/deps/zz_os-2fd76523e1f9f4f6: src/main.rs
+
+/Users/Apple/Desktop/Some/my-rust-os/zz_os/target/debug/deps/zz_os-2fd76523e1f9f4f6.d: src/main.rs
+
+src/main.rs:
